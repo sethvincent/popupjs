a17a67e2fc60a65f5f7be0fcb986650e59597e6e
sethvincent <sethvincent@gmail.com>
2015-06-07 12:25:12 -0700

    actually center the box

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require('fs')
 var path = require('path')
 var Emitter = require('component-emitter')
-var on = require('component-delegate').bind
 var template = require('lodash.template')
+var select = require('dom-select')
 var extend = require('extend')
+var on = require('dom-event')
 
 var popupTemplate = template(fs.readFileSync(__dirname + '/popup.html', 'utf8'))
 
@@ -12,15 +13,15 @@ module.exports = Popup
 function Popup (opts) {
   if (!(this instanceof Popup)) return new Popup(opts)
   var self = this
-  
+
   this.template = popupTemplate || opts.template
   this.el = opts.el || document.body
   this.id = opts.id
-  
+  this.center = opts.center || true
   this.data = { content: opts.content || '' }
-  
+
   if (opts.buttons === false) this.data.buttons = null
-  
+
   else {
     this.data.buttons = opts.buttons || [{
       text: 'OK',
@@ -30,8 +31,12 @@ function Popup (opts) {
       }
     }]
   }
-  
+
   this._create()
+  this.on('show', function () {
+    if (this.center) this._center()
+    this._createEventListeners()
+  })
 }
 
 Emitter(Popup.prototype)
@@ -41,25 +46,32 @@ Popup.prototype._create = function () {
   this.html.id = this.id
   this.html.className = 'popupjs-background'
   this.html.innerHTML = this.template(this.data)
-  this._createEventListeners()
+}
+
+Popup.prototype._center = function () {
+  var wrapper = select('.popupjs-wrapper')
+  wrapper.style.marginLeft = -(wrapper.offsetWidth / 2) + 'px'
+  wrapper.style.marginTop = -(wrapper.offsetHeight / 2) + 'px'
+  wrapper.style.top = '50%'
+  wrapper.style.left = '50%'
 }
 
 Popup.prototype._createEventListeners = function () {
   var self = this
-  
-  on(document.body, '.popupjs-close', 'click', function (e) {
+
+  on(select('.popupjs-close'), 'click', function (e) {
     self.remove()
   })
-  
-  on(document.body, '.popupjs-background', 'click', function (e) {
+
+  on(select('.popupjs-background'), 'click', function (e) {
     if (e.target.className === 'popupjs-background') {
       self.remove()
     }
   })
-  
+
   if (this.data.buttons) {
     this.data.buttons.forEach(function (button) {
-      on(document.body, '.popupjs-button-' + button.className, 'click', function (e) {
+      on(select('.popupjs-button-' + button.className), 'click', function (e) {
         if (!button.fn) return self.remove()
         button.fn(e)
       })
